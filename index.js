b7bd96c7d0e054469aae8904452b721f8de15dd6
Jacob <jacobpayne007@gmail.com>
2024-04-14 12:16:20 -0500

    need more practice here

--- index.js
+++ index.js
@@ -11,6 +11,33 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+const titleCasedTutorials = tutorials.map(tutorial=> {
+  const words = tutorial.split(' ');
+
+  const titleCasedWords = words.map(word => {
+    const lowerCaseWord = word.toLowerCase();
+    const firstLetter = lowerCaseWord.charAt(0).toUpperCase();
+    const restOfWord = lowerCaseWord.slice(1);
+
+    return firstLetter + restOfWord;
+  });
+  return titleCasedWords.join(' ');
+});
+
+
 const titleCased = () => {
-  return tutorials
+   let done = tutorials.map(tutorial=> {
+    const words = tutorial.split(' ');
+  
+    const titleCased = words.map(word => {
+      const firstLetter = word.charAt(0).toUpperCase();
+      const restOfWord = word.slice(1);
+  
+      return firstLetter + restOfWord;
+    });
+    return titleCased.join(' ');
+  });
+  return done
 }
+
+console.log(titleCased())
